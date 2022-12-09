73730311ba788a9becc3cd8e6820a45599072aa3
Jacob Cofman <jcofman@inovex.de>
2022-12-09 01:17:04 +0100

    chore: solve day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,27 @@
 fun main() {
+
+    fun parse(input: List<String>, n: Int): Int {
+        val listOfElfs: MutableList<Int> = mutableListOf()
+        var calories = 0;
+        input.forEach {
+            if (it.isEmpty()) {
+                listOfElfs.add(calories)
+                calories = 0
+            } else {
+                calories += it.toInt()
+            }
+        }
+        return listOfElfs.sortedDescending().take(n).sum()
+
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return parse(input, 1)
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return parse(input, 3)
     }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     println(part1(input))
